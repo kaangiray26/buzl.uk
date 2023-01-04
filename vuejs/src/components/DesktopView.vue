1ce03955dbb96968c2c433b584bc91c9c238cbe2
Kaan Giray Buzluk <kaangiray26@protonmail.com>
2023-01-04 15:04:16 +0300

    clean

--- vuejs/src/components/DesktopView.vue
+++ vuejs/src/components/DesktopView.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="d-flex flex-column hide-on-mobile">
         <div class="container pb-4" style="height: 50vh !important;">
-            <div class="row gx-4 h-100">
+            <div class="row gx-4 h-100 pb-4">
                 <div class="col-6 h-100">
                     <Project :data="projects[0]"></Project>
                 </div>
@@ -21,7 +21,7 @@
             </div>
         </div>
         <div class="container pb-4">
-            <div class="row row-cols-4 g-4 mb-2">
+            <div class="row row-cols-4 g-4 pb-4">
                 <div class="col" v-for="project in projects.slice(4, -1)">
                     <Project :data="project"></Project>
                 </div>
@@ -29,9 +29,11 @@
                     <Project :data="project"></Project>
                 </div>
             </div>
-            <span class="text-decoration-underline">
-                All the project images you see on this page are created with DALL-E by OpenAI.
-            </span>
+            <div class="mx-4 pb-4">
+                <span class="text-white">
+                    Some of the project images you see on this page are created with DALL-E by OpenAI.
+                </span>
+            </div>
         </div>
     </div>
 </template>
--- vuejs/src/components/Project.vue
+++ vuejs/src/components/Project.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="card rounded-0 clickable mx-4" @click="openProject">
+    <div class="hide-on-mobile card h-100 rounded-0 clickable mx-4" @click="openProject">
         <img :src="props.data.image" class="project-image img-thumbnail" :alt="props.data.name">
         <div class="card-img-overlay rounded-0 d-flex align-items-center bottom-text p-4">
             <div class="mt-auto">
@@ -10,7 +10,18 @@
             </div>
         </div>
     </div>
-    <div class="mx-4 mb-4">
+    <div class="hide-on-desktop card rounded-0 clickable mx-4" @click="openProject">
+        <img :src="props.data.image" class="project-image img-thumbnail" :alt="props.data.name">
+        <div class="card-img-overlay rounded-0 d-flex align-items-center bottom-text p-4">
+            <div class="mt-auto">
+                <div class="d-flex flex-column text-white">
+                    <span class="fs-4 fw-bolder text-capitalize">{{ props.data.name }}</span>
+                    <span class="hide-on-mobile">{{ props.data.description }}</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="mx-4 mb-4 hide-on-desktop">
         <span>{{ props.data.description }}</span>
     </div>
 </template>
